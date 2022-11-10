374e807b4954bd8da51ead4b42c6c96f6059cd36
oktembaris77 <oktembaris77@gmail.com>
2022-11-10 18:58:26 +0300

    update

--- index.js
+++ index.js
@@ -7,26 +7,8 @@ const path = require('path');
 const inputPath = 'cat.jpg';
 const formData = new FormData();
 formData.append('size', 'auto');
-formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));*/
 
-axios({
-  method: 'post',
-  url: 'https://api.remove.bg/v1.0/removebg',
-  data: formData,
-  responseType: 'arraybuffer',
-  headers: {
-    ...formData.getHeaders(),
-    'X-Api-Key': '',
-  },
-  encoding: null
-})
-.then((response) => {
-  if(response.status != 200) return console.error('Error:', response.status, response.statusText);
-  fs.writeFileSync("cat-no-bg.png", response.data);
-})
-.catch((error) => {
-    return console.error('Request failed:', error);
-});*/
 
 ///////////////////////////////////////////////////////
 const axios = require('axios');
@@ -37,6 +19,26 @@ const inputPath = 'cat.jpg';
 const formData = new FormData();
 formData.append('size', 'auto');
 formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+alert("aa: " + process.env.KEY);
+axios({
+  method: 'post',
+  url: 'https://api.remove.bg/v1.0/removebg',
+  data: formData,
+  responseType: 'arraybuffer',
+  headers: {
+    ...formData.getHeaders(),
+    'X-Api-Key': process.env.KEY,
+  },
+  encoding: null
+})
+.then((response) => {
+  if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+  fs.writeFileSync("cat-no-bg.png", response.data);
+})
+.catch((error) => {
+    return console.error('Request failed:', error);
+});
+
 
 ////////////////////////////////////////////////////////////
 ///
