4f4166f3da61cb7efcca717bd358379a0e3b826e
oktembaris77 <oktembaris77@gmail.com>
2022-11-11 23:12:41 +0300

    update

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
+// Requires "axios" and "form-data" to be installed (see https://www.npmjs.com/package/axios and https://www.npmjs.com/package/form-data)
+/*const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = 'cat.jpg';
+const formData = new FormData();
+formData.append('size', 'auto');
+formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));*/
+
+
+///////////////////////////////////////////////////////
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
+const inputPath = 'cat.jpg';
+const formData = new FormData();
+
+
+////////////////////////////////////////////////////////////
+///
+
 const express = require("express");
-const path = require("path");
 const app = express();
+app.use(express.json());
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-
-app.get("/", function(req, res){
+app.get("/", (req, res)=>{
   res.send("Hello World");
+
+  formData.append('size', 'auto');
+formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+
+axios({
+  method: 'post',
+  url: 'https://api.remove.bg/v1.0/removebg',
+  data: formData,
+  responseType: 'arraybuffer',
+  headers: {
+    ...formData.getHeaders(),
+    'X-Api-Key': process.env.KEY,
+  },
+  encoding: null
 })
-
-io.on("connection", (socket) => {
-    console.log(socket.id + " New connection.");
-
-    socket.on("disconnect", () =>{
-        console.log(socket.id  + " disconnected");
-    });
-
-    socket.on("new message", msg => {
-        console.log("new message on the server", msg);
-        io.emit("incomming", msg);
-    });
+.then((response) => {
+  if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+  fs.writeFileSync("cat-no-bg.png", response.data);
+})
+.catch((error) => {
+    return console.error('Request failed:', error);
 });
 
+ 
+});
 
-server.listen(3002, () => {
-    console.log("Server started...");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server");
 });
