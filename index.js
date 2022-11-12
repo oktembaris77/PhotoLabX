cea878caf91617cbf6e29582a0eea50b6354ecb4
oktembaris77 <oktembaris77@gmail.com>
2022-11-12 16:34:38 +0300

    update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ router.get("/", function(req, res){
   res.sendFile(path.join(__dirname+'/app/index.html'));
 });
 
-router.get("/app.js", function(req, res){
+app.get("/app", function(req, res){
   res.render(path.join(__dirname+'/app/app.js'));
 });
 
