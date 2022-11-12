46eaad4bad1eaceb3ee6b6f28b3d682b2e1b1ea2
oktembaris77 <oktembaris77@gmail.com>
2022-11-12 15:09:05 +0300

    update

---
+++ server/index.js
@@ -0,0 +1,30 @@
+/*const socketIO = require('socket.io');
+const io = socketIO(5500);
+
+io.on('connection', (socket) => {
+  console.log('Bir kullanıcı bağlandı: ' + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Bir kullanıcı ayrıldı: ' + socket.id);
+  });
+});*/
+
+
+const http = require('http').createServer();
+
+const io = require('socket.io')(http,{
+  cors: {origin: "*"}
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message',`${socket.id} said ${message}`);
+
+  });
+
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080'));
